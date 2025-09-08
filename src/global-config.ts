741d6c9f880f0139d7745f4589335ee6c63a22f8
qiming.li <qiming.li@adspower.net>
2025-09-08 21:48:56 +0800

    docs: 更新应用名称为IDNYX

--- src/global-config.ts
+++ src/global-config.ts
@@ -32,7 +32,7 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'Minimal UI',
+  appName: 'IDNYX',
   appVersion: packageJson.version,
   serverUrl: import.meta.env.VITE_SERVER_URL ?? '',
   assetsDir: import.meta.env.VITE_ASSETS_DIR ?? '',
