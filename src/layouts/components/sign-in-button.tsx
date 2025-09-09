c3262489499b26f5fe717094a74aef88352a8917
qiming.li <qiming.li@adspower.net>
2025-09-10 00:17:04 +0800

    refactor(auth): remove unused import of paths in sign-in button component

--- src/layouts/components/sign-in-button.tsx
+++ src/layouts/components/sign-in-button.tsx
@@ -2,7 +2,6 @@ import type { ButtonProps } from '@mui/material/Button';
 
 import Button from '@mui/material/Button';
 import { RouterLink } from 'src/routes/components';
-import { paths } from 'src/routes/paths';
 import { CONFIG } from 'src/global-config';
 import { useAuthContext } from 'src/auth/hooks';
 
